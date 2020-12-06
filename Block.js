c58971d68aa38c82bb587a4ff87e629414b5215a
git-ArnavMishra <68264157+git-ArnavMishra@users.noreply.github.com>
2020-12-05 18:35:58 -0800

    Update Block.js

--- Block.js
+++ Block.js
@@ -1,7 +1,7 @@
 class Block extends BaseClass {
     constructor(x, y, width, height){
       super(x,y,width,height);
-      this.image = loadImage("sprites/box.png");
+      this.image = loadImage("Sprites/Box.png");
       this.visibility = 255;
       this.visible = true;
     }
@@ -20,7 +20,7 @@ class Block extends BaseClass {
     else{
       World.remove(world, this.body);
       tint(255,this.visibility);
-       this.image = loadImage("sprites/box.png");
+       this.image = loadImage("Sprites/box.png");
       //this.image(this.image, this.body.position.x, this.body.position.y, 50, 50);
 
 
